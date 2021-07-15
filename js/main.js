57ccb42b38f63bd497c0f77978801f99018f768e
amedeo <amedeopasanisi97@gmail.com>
2021-07-15 14:44:32 +0200

    fine esercizio

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+var kmDaPercorrere = prompt("Inserisci il numero di chilometri che vuoi percorrere");
+console.log("numero di chilometri da percorrere " + kmDaPercorrere);
+
+var etaPasseggero = prompt("inserisci quanti anni hai");
+console.log("età del passeggero " + etaPasseggero);
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km).
+var euroAlKm = 0.21;
+
+// con queste informazioni calcolare il prezzo totale del viaggio.
+var prezzoDelViaggio = kmDaPercorrere * euroAlKm;
+// con massimo due decimali, per indicare centesimi.
+var prezzoDelViaggioArrotondato = Math.round(prezzoDelViaggio * 100) / 100;
+console.log("prezzo del biglietto senza sconti applicati € " + prezzoDelViaggio);
+
+// va applicato uno sconto del 20% per i minorenni.
+var scontoMinorenni = 20;
+
+// va applicato uno sconto del 40% per gli over 65.
+var scontoOver65 = 40;
+
+// output del prezzo finale.
+if (etaPasseggero < 18) {
+
+    var prezzoBigliettoMinorenni = prezzoDelViaggio - ((prezzoDelViaggio * scontoMinorenni) / 100);
+    // con massimo due decimali, per indicare centesimi.
+    var prezzoBigliettoMinorenniArrotondato = Math.round(prezzoBigliettoMinorenni * 100) / 100;
+    console.log("prezzo del biglietto con sconto minorenni applicato € " + prezzoBigliettoMinorenniArrotondato)
+
+} if (etaPasseggero > 65) {
+
+    var prezzoBigliettoOver65 = prezzoDelViaggio - ((prezzoDelViaggio * scontoOver65) / 100);
+    // con massimo due decimali, per indicare centesimi.
+    var prezzoBigliettoOver65Arrotondato = Math.round(prezzoBigliettoOver65 * 100) / 100;
+    console.log("prezzo del biglietto con sconto over 65 applicato € " + prezzoBigliettoOver65Arrotondato)
+
+}
+
+
