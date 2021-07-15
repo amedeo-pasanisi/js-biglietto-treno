210e6b419bfc289ef36f56360a3d8a774b3be861
amedeo <amedeopasanisi97@gmail.com>
2021-07-15 19:21:36 +0200

    correzione parseInt/Float, variabili, else if

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-var kmDaPercorrere = prompt("Inserisci il numero di chilometri che vuoi percorrere");
+var kmDaPercorrere = parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere"));
 console.log("numero di chilometri da percorrere " + kmDaPercorrere);
 
-var etaPasseggero = prompt("inserisci quanti anni hai");
+var etaPasseggero = parseInt(prompt("inserisci quanti anni hai"));
 console.log("età del passeggero " + etaPasseggero);
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km).
@@ -10,9 +10,11 @@ var euroAlKm = 0.21;
 
 // con queste informazioni calcolare il prezzo totale del viaggio.
 var prezzoDelViaggio = kmDaPercorrere * euroAlKm;
+console.log("prezzo del biglietto senza sconti applicati: € " + prezzoDelViaggio);
+
 // con massimo due decimali, per indicare centesimi.
-var prezzoDelViaggioArrotondato = Math.round(prezzoDelViaggio * 100) / 100;
-console.log("prezzo del biglietto senza sconti applicati € " + prezzoDelViaggio);
+prezzoDelViaggio = Math.round(prezzoDelViaggio * 100) / 100;
+console.log("prezzo arrotondato a due cifre decimali: € " + prezzoDelViaggio);
 
 // va applicato uno sconto del 20% per i minorenni.
 var scontoMinorenni = 20;
@@ -24,16 +26,20 @@ var scontoOver65 = 40;
 if (etaPasseggero < 18) {
 
     var prezzoBigliettoMinorenni = prezzoDelViaggio - ((prezzoDelViaggio * scontoMinorenni) / 100);
-    // con massimo due decimali, per indicare centesimi.
-    var prezzoBigliettoMinorenniArrotondato = Math.round(prezzoBigliettoMinorenni * 100) / 100;
-    console.log("prezzo del biglietto con sconto minorenni applicato € " + prezzoBigliettoMinorenniArrotondato)
+    console.log("prezzo del biglietto con sconto minorenni applicato: € " + prezzoBigliettoMinorenni)
 
-} if (etaPasseggero > 65) {
+    // con massimo due decimali, per indicare centesimi.
+    prezzoBigliettoMinorenni = Math.round(prezzoBigliettoMinorenni * 100) / 100;
+    console.log("prezzo arrotondato a due cifre decimali: € " + prezzoBigliettoMinorenni)
+
+} else if (etaPasseggero > 65) {
 
     var prezzoBigliettoOver65 = prezzoDelViaggio - ((prezzoDelViaggio * scontoOver65) / 100);
+    console.log("prezzo del biglietto con sconto over 65 applicato: € " + prezzoBigliettoOver65)
+
     // con massimo due decimali, per indicare centesimi.
-    var prezzoBigliettoOver65Arrotondato = Math.round(prezzoBigliettoOver65 * 100) / 100;
-    console.log("prezzo del biglietto con sconto over 65 applicato € " + prezzoBigliettoOver65Arrotondato)
+    prezzoBigliettoOver65 = Math.round(prezzoBigliettoOver65 * 100) / 100;
+    console.log("prezzo arrotondato a due cifre decimali: € " + prezzoBigliettoOver65)
 
 }
 
